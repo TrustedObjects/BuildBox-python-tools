74dbe7117fbe01f3e4c4eaf2b420c88b882c9a2b
Mathieu ABATI <m.abati@trusted-objects.com>
2026-04-16 15:07:39 +0200

    Add license information

--- load.sh
+++ load.sh
@@ -1,3 +1,19 @@
+# Python tools for BuildBox - load script
+# Copyright (C) 2023-2026 Trusted Objects
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2, as published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# <https://www.gnu.org/licenses/>.
+
 OPTS=
 
 # Are system site packages allowed?
